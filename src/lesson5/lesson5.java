9bcb8146c9bf99923ff540a5c5ccd9e67f04c0f8
perfanat <perfanat@yandex.ru>
2019-04-13 13:28:21 +0300

    дз 2

--- src/lesson5/lesson5.java
+++ src/lesson5/lesson5.java
@@ -1,4 +1,64 @@
 package lesson5;
 
 public class lesson5 {
+    private static final int size = 10000000;
+    private static final int h = size / 2;
+
+    public static void main(String[] args) {
+        float[] arr = new float[size];
+        float[] arr1 = new float[h];
+        float[] arr2 = new float[h];
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < arr2.length; i++) {
+                    arr2[i]=(float)(arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < arr2.length; i++) {
+                    arr2[i]=(float)(arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=1;
+        }
+
+        long a = System.currentTimeMillis();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=(float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        long b = System.currentTimeMillis();
+
+        System.out.printf("время выполнения основной операции - %d%n",(b-a));
+
+        long c = System.currentTimeMillis();
+
+        System.arraycopy(arr,0,arr1,0, h);
+        System.arraycopy(arr,h,arr2,0, h);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+
+        long d = System.currentTimeMillis();
+
+        System.out.printf("время выполнения поделённой операции - %d",(d-c));
+    }
 }
