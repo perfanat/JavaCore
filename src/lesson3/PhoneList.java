36023a6b57ae0b41b222b5e38bc0de6e12069c83
perfanat <perfanat@yandex.ru>
2019-04-13 12:08:09 +0300

    дз 2

--- src/lesson3/PhoneList.java
+++ src/lesson3/PhoneList.java
@@ -1,6 +1,7 @@
 package lesson3;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -8,6 +9,9 @@ public class PhoneList {
     private static Map<String, Set<String>>phLis=new HashMap<>();
 
     public static void main(String[] args) {
+        phLis.put("Иванов", new HashSet<>());
+        phLis.put("Петров", new HashSet<>());
+        phLis.put("Сидоров", new HashSet<>());
         phLis.get("Иванов").add("123456");
         phLis.get("Петров").add("123455");
         phLis.get("Сидоров").add("123454");
@@ -17,7 +21,7 @@ public class PhoneList {
         phLis.get("Петров").add("123452");
         printList(phLis);
         System.out.println("--------");
-        findPhone("Иванов");
+        findPhone("Петров");
     }
 
     private static void printList(Map<String, Set<String>> hashMap){
@@ -30,8 +34,10 @@ public class PhoneList {
 
     private static void findPhone(String str){
         for(Map.Entry<String, Set<String>> e: phLis.entrySet()) {
-            if (e.getValue().equals(str))
-            System.out.println(e.getValue()+": "+e.getKey());
+            if (e.getKey().equals(str))
+                for(String s : e.getValue()) {
+                    System.out.println(e.getKey()+" - "+s);
+                }
         }
     }
 }
