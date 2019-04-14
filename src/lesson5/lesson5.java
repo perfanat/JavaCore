81e3dfc6d687e3feac5917fe527e90109bc41bf6
perfanat <perfanat@yandex.ru>
2019-04-14 20:44:54 +0300

    дз 2

--- src/lesson5/lesson5.java
+++ src/lesson5/lesson5.java
@@ -12,8 +12,8 @@ public class lesson5 {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < arr2.length; i++) {
-                    arr2[i]=(float)(arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                for (int i = 0; i < arr1.length; i++) {
+                    arr1[i]=(float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
             }
         });
